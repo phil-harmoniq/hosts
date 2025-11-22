e86c1a6864ff28e8b698a9ce551f5337bc5052b5
Phil Hawkins <ph.hawkins@outlook.com>
2025-11-22 17:21:18 -0500

    Config

--- rpi/init-rpi-alma.sh
+++ rpi/init-rpi-alma.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+# script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 base_url="https://raw.githubusercontent.com/phil-harmoniq/hosts/refs/heads/main/rpi"
 
 main()
 {
     add_shell_config
-    # install_packages
+    install_packages
+    enable_services
+    configure_podman
 }
 
 add_shell_config()
@@ -20,19 +22,45 @@ add_shell_config()
     curl "$base_url/shell/.bashrc.d/aliases" > "$tmp_dir/shell/.bashrc.d/aliases"
     curl "$base_url/shell/.bashrc.d/prompt" > "$tmp_dir/shell/.bashrc.d/prompt"
     curl "$base_url/shell/.inputrc" > "$tmp_dir/shell/.inputrc"
-    ls -lhaF "$tmp_dir/shell"
-    # DEST_BASH_CONFIG_DIR="$HOME/.bashrc.d/"
 
-    # mkdir -p "$DEST_BASH_CONFIG_DIR"
-    # cp "$SOURCE_BASH_CONFIG_DIR/aliases" "$DEST_BASH_CONFIG_DIR"
-    # cp "$SOURCE_BASH_CONFIG_DIR/prompt" "$DEST_BASH_CONFIG_DIR"
-    # cp "$script_dir/.inputrc" "$HOME"
+    echo "Adding shell config to current user"
+    mkdir -p "$HOME/.bashrc.d/"
+    cp "$tmp_dir/shell/.bashrc.d/aliases" "$HOME/.bashrc.d"
+    cp "$tmp_dir/shell/.bashrc.d/prompt" "$HOME/.bashrc.d"
+    cp "$tmp_dir/shell/.inputrc" "$HOME"
 }
 
 install_packages()
 {
-    dnf install epel-release
-    dnf install podman git btop tldr shellcheck fastfetch cockpit-files
+    echo "Installing default environment packages"
+
+    dnf install epel-release -y
+    dnf install \
+        podman \
+        git \
+        btop \
+        tldr \
+        shellcheck \
+        fastfetch \
+        cockpit \
+        cockpit-files \
+        cockpit-podman \
+        cockpit-storaged \
+        samba \
+        samba-client \
+        cifs-utils \
+        nfs-utils \
+        -y
+}
+
+enable_services()
+{
+    systemctl enable --now cockpit
+}
+
+configure_podman()
+{
+    loginctl enable-linger devops
 }
 
 main
